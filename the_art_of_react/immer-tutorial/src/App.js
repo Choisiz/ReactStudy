f007ed4feb9fb842961fea3e24a4c232d3643456
KyungYeul Choi <dnjsvltm201311681@gmail.com>
2023-01-18 17:26:01 +0900

    immer 적용전

---
+++ the_art_of_react/immer-tutorial/src/App.js
@@ -0,0 +1,80 @@
+import React, { useCallback, useRef, useState } from "react";
+
+const App = () => {
+  const nextId = useRef(1);
+  const [form, setForm] = useState({ name: "", username: "" });
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null,
+  });
+
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setForm({ ...form, [name]: [value] }); //input 여러개라서 변수명으로 사용
+    },
+    [form]
+  );
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username,
+      };
+
+      setData({
+        ...data,
+        array: data.array.concat(info),
+      });
+      setForm({
+        name: "",
+        username: "",
+      });
+      nextId.current += 1;
+    },
+    [data, form.name, form.username]
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setData({
+        ...data,
+        array: data.array.filter((info) => info.id !== id),
+      });
+    },
+    [data]
+  );
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="username"
+          placeholder="id"
+          value={form.value}
+          onChange={onChange}
+        />
+        <input
+          name="name"
+          placeholder="name"
+          value={form.name}
+          onChange={onChange}
+        />
+        <button type="submit">등록</button>
+      </form>
+      <div>
+        <ul>
+          {data.array.map((info) => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username} ({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
