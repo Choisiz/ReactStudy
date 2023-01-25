13757791450b2dc8cfce1d7b23c5877331f9eb6f
KyungYeul Choi <dnjsvltm201311681@gmail.com>
2023-01-25 20:12:41 +0900

    switch

--- the_art_of_react/router-toturial/src/App.js
+++ the_art_of_react/router-toturial/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Profiles from "./Profiles";
@@ -22,10 +22,20 @@ const App = () => {
           <Link to="/history">history에제</Link>
         </li>
       </ul>
-      <Route path="/" component={Home} exact={true} />
-      <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profiles" component={Profiles} />
-      <Route path="/history" component={HistorySample} />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path={["/about", "/info"]} component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>존재하지않는페이지입니다.</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 };
