d2c2144ee190212f5a5d356e2a795dfb766fdaad
KyungYeul Choi <dnjsvltm201311681@gmail.com>
2023-01-25 20:17:58 +0900

    NavLink

--- the_art_of_react/router-toturial/src/Profiles.js
+++ the_art_of_react/router-toturial/src/Profiles.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
+import { Link, NavLink, Route } from "react-router-dom";
 import Profile from "./Profile";
 
 const Profiles = () => {
+  const activeStyle = {
+    background: "black",
+    color: "white",
+  };
   return (
     <div>
       <h3>커피목록</h3>
       <ul>
         <li>
-          <Link to="/profiles/cafe">카페</Link>
+          <NavLink activeStyle={activeStyle} to="/profiles/cafe" active>
+            카페
+          </NavLink>
         </li>
         <li>
-          <Link to="/profiles/menu">메뉴</Link>
+          <NavLink activeStyle={activeStyle} to="/profiles/menu" active>
+            메뉴
+          </NavLink>
         </li>
       </ul>
       <Route path="/profiles" exact render={() => <div>선택하세요</div>} />
