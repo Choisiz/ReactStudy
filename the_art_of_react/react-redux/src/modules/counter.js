e5431a3c49ed877e6c34e536f535a84f2beddc55
KyungYeul Choi <dnjsvltm201311681@gmail.com>
2023-03-09 15:28:11 +0900

    redux 상태관리하기 redux-action로 변경

--- the_art_of_react/react-redux/src/modules/counter.js
+++ the_art_of_react/react-redux/src/modules/counter.js
@@ -1,10 +1,13 @@
+import { createAction, handleActions } from "redux-actions";
 //action
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
 //action function
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+//export const increase = () => ({ type: INCREASE });
+//export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 //init
 const initialState = {
@@ -12,19 +15,27 @@ const initialState = {
 };
 
 //reducer
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState
+);
 
 export default counter;
